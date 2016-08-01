455850cb9faca118da3aed08ee8a4cdaa8500e2c
masuda <masuda.shigeki@synergy101.jp>
2016-08-01 23:39:58 +0900

    deviseの設定追加

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Rails.application.routes.draw do
+
+  root 'boards#index'
   resources :boards
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  devise_for :users, controllers: {
+      omniauth_callbacks: 'users/omniauth_callbacks'
+  }
 end
