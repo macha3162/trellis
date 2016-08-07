bb5bd2203eb45e55d94e1cfc241260fec1855857
増田　茂樹 <shimasud@MBP-15UAC-219.local>
2016-08-07 17:45:21 +0900

    並び替えのテスト追加.

--- spec/factories/cards.rb
+++ spec/factories/cards.rb
@@ -4,4 +4,22 @@ FactoryGirl.define do
     description "MyText"
     ordinal 1
   end
+
+  factory :card1, class: Card do
+    title 'カード'
+    description "MyText"
+    ordinal 1
+  end
+
+  factory :card2, class: Card do
+    title 'カード'
+    description "MyText"
+    ordinal 1
+  end
+
+  factory :card3, class: Card do
+    title 'カード'
+    description "MyText"
+    ordinal 1
+  end
 end
--- spec/models/card_spec.rb
+++ spec/models/card_spec.rb
@@ -8,4 +8,19 @@ RSpec.describe Card, type: :model do
       expect(card.errors[:title]).to include('を入力してください')
     end
   end
+
+  describe 'sort' do
+    it 'カードの並び替えが行えること' do
+      user = create(:user)
+      list = create(:list)
+      list2 = create(:list2)
+
+      card1 = create(:card1, list: list, user: user)
+      card2 = create(:card2, list: list, user: user)
+      card3 = create(:card3, list: list2, user: user)
+
+      Card.sort(list, [card3.id, card1.id, card2.id])
+      expect(list.cards.order(:ordinal)).to eq [card3, card1, card2]
+    end
+  end
 end
